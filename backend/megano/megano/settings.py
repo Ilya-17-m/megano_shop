062b0a28aca4309f60d660fb35feda194cd5ee2c
user <ilya.melnichuk.2007@mail.ru>
2026-05-03 15:03:45 +0500

    commit

--- backend/megano/megano/settings.py
+++ backend/megano/megano/settings.py
@@ -124,8 +124,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10,
 }
 
 # Internationalization
